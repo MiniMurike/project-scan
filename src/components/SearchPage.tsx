c6c994ccab5ffe0f119e26c37a79a1da476509d4
Usver <3058viktorrus@gmail.com>
2024-05-30 06:44:19 +0500

    fixed inn validation

--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -64,12 +64,7 @@ function SearchPage(props) {
                 inn.split("").map((value, index) => {
                     if (index != 9) sumResult += value * numCoefficient[index];
                 })
-                console.log(`sumresult = ${sumResult}`);
                 sumResult %= 11;
-                console.log(`sumresult % 11 = ${sumResult}`)
-                console.log(`last digit = ${inn[9]}`)
-                console.log(typeof inn[9])
-                console.log(typeof sumResult)
                 if (inn[9] != sumResult % 10) throw "";
 
                 updateValidationFieldsState({
